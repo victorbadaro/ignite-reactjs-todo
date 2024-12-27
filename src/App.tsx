7aa41af929b9f59dfb6cba4a4e5ba9661671c565
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-27 10:16:37 -0300

    feat: add form to create new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { PlusCircle } from '@phosphor-icons/react';
+import type { FormEvent } from 'react';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
+
 export function App() {
+	function handleCreateTask(event: FormEvent) {
+		event.preventDefault();
+	}
+
 	return (
 		<>
 			<Header />
-			<h1>Hello, World!</h1>
+
+			<main className={styles.wrapper}>
+				<form onSubmit={handleCreateTask}>
+					<input type="text" placeholder="Adicione uma nova tarefa" />
+
+					<button type="submit">
+						<span>Criar</span>
+						<PlusCircle size={16} weight="bold" />
+					</button>
+				</form>
+			</main>
 		</>
 	);
 }
