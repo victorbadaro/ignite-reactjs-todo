5aee39c5b1911803bcae84249c96dac629e8601b
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-29 10:14:54 -0300

    feat: add different icons to be shown while hovering todo checkbox

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,6 +1,7 @@
 import type { Task as TaskType } from '@/contexts/TasksContext';
 import { useTasks } from '@/hooks/useTasks';
 import { CheckCircle, Circle, Trash } from '@phosphor-icons/react';
+import { useState } from 'react';
 
 import styles from './styles.module.css';
 
@@ -8,6 +9,7 @@ interface TaskProps extends TaskType {}
 
 export function Task({ id, description, isDone }: TaskProps) {
 	const { toggleCheckTask, deleteTask } = useTasks();
+	const [isMouseOver, setIsMouseOver] = useState(false);
 
 	function handleCheckTask() {
 		toggleCheckTask(id);
@@ -17,10 +19,24 @@ export function Task({ id, description, isDone }: TaskProps) {
 		deleteTask(id);
 	}
 
+	function handleMouseOver() {
+		setIsMouseOver(true);
+	}
+
+	function handleMouseLeave() {
+		setIsMouseOver(false);
+	}
+
 	return (
 		<div className={isDone ? `${styles.task} ${styles['checked-task']}` : styles.task}>
-			<button type="button" onClick={handleCheckTask}>
-				{isDone ? <CheckCircle size={17} weight="fill" color="#5e60ce" /> : <Circle size={17} weight="bold" color="#4ea8de" />}
+			<button type="button" onClick={handleCheckTask} onMouseOver={handleMouseOver} onFocus={handleMouseOver} onMouseLeave={handleMouseLeave}>
+				{isDone ? (
+					<CheckCircle size={17} weight="fill" color={isMouseOver ? '#8284fa' : '#5e60ce'} />
+				) : isMouseOver ? (
+					<Circle size={17} weight="duotone" color="#1e6f9f" />
+				) : (
+					<Circle size={17} weight="bold" color="#4ea8de" />
+				)}
 			</button>
 			<p>{description}</p>
 			<button type="button" title="Excluir" onClick={handleDeleteTask}>
